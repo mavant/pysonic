6071601ab0279c1a5fbd1032ec01c2f00bd10644
Matthew Avant <matthew.avant@gmail.com>
2014-04-01 21:04:37 -0400

    Cleaning up and organizing

--- main.clj
+++ main.clj
@@ -3,23 +3,6 @@
   ([l] (apply str (interpose " " (macroexpand-1 `(s-expr->semantic-whitespace ~l 1))))) 
   ([l n] (map (fn [x] (if (list? x) (apply str "\n" (apply str (repeat n "\t")) (interpose " " (macroexpand-1 `(s-expr->semantic-whitespace ~x ~(+ n 1)))) ) x)) l)))
 
-
-(def pre-example '(s-expr->semantic-whitespace (defn tabs->parens [c] (map add-parens (rest (split-on-newlines c)) (list-deltas (code->numtabs c))))))
-(def example (macroexpand-1 pre-example))
-;(println pre-example)
-(println example)
-;=>
-;defntabs->parens[c]
-;	map add-parens 
-;		rest 
-;			split-on-newlines c 
-;		list-deltas 
-;			code->numtabs c
-;
-;
-;
-
-
 (defn split-on-newlines "Splits a string into a list of strings by any newline characters." [s] (clojure.string/split s #"\n"))
 
 (defn indented? "Returns true if the string has at least one tab character, otherwise false." [s] (not (nil? (re-find #"\t" s))))
@@ -30,4 +13,8 @@
 
 (defn whitespace->s-exprs "Converts from a string of whitespace-syntax code to an evaluable S-expression." [s] (-> s whitespace->parentheses read-line))
 
-(print (whitespace->s-exprs example))
+
+(comment "And below we have some examples.")
+(def s-expression-example '(s-expr->semantic-whitespace (defn tabs->parens [c] (map add-parens (rest (split-on-newlines c)) (list-deltas (code->numtabs c))))))
+(def semantic-whitespace-example (macroexpand-1 pre-example))
+(def back-to-s-expr-example (whitespace->s-exprs example))
