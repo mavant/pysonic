3052f5e9303e5c7476440678029b1f00cb209773
Matthew Avant <matthew.avant@gmail.com>
2014-04-01 20:39:22 -0400

    Conversion whitespace->s-expr is now functional.

--- main.clj
+++ main.clj
@@ -6,7 +6,7 @@
 
 (def pre-example '(s-expr->semantic-whitespace (defn tabs->parens [c] (map add-parens (rest (split-on-newlines c)) (list-deltas (code->numtabs c))))))
 (def example (macroexpand-1 pre-example))
-(println pre-example)
+;(println pre-example)
 (println example)
 ;=>
 ;defntabs->parens[c]
@@ -20,17 +20,12 @@
 ;
 
 
-(defn split-on-newlines [s] (clojure.string/split s #"\n"))
+(defn split-on-newlines "Splits a string into a list of strings by any newline characters." [s] (clojure.string/split s #"\n"))
 
-(defn indented? [s] (not (nil? (re-find #"\t" s))))
+(defn indented? "Returns true if the string has at least one tab character, otherwise false." [s] (not (nil? (re-find #"\t" s))))
 
-(defn tabs->lists [l] (reduce (fn [a b] (if (indented? b) 
-                                          (conj (pop a) (conj (peek a) (clojure.string/replace-first b #"\t" "")))
-                                          (conj a [b]))) [] l))
-(defn disp [x] (if (list? x) (map tabs->lists x) x))
+(defn remove-first-tab "Removes the first tab character from a string." [x] (clojure.string/replace-first x #"\t" ""))
 
-(defn whitespace->s-exprs [s] (-> s split-on-newlines tabs->lists))
+(defn whitespace->s-exprs "Converts code from semantic-whitespace form to S-expression form. Accepts either a list of lines or a string." [l] (if (string? l) (whitespace->s-exprs (split-on-newlines l)) (map (fn [x] (concat (vec (first x)) (vec (whitespace->s-exprs (map remove-first-tab (second x)))))) (partition-all 2 (partition-by indented? l)))))
 
-;(prn (whitespace->s-exprs example))
-
-(prn (disp (split-on-newlines example)))
+(prn (whitespace->s-exprs example))
