2d943bb5d4ad1bedc1add9924d7c4a5190378784
Matthew Avant <matthew.avant@gmail.com>
2014-04-01 21:15:08 -0400

    Changed read-line to read-string, and found a misplaced close-paren.

--- main.clj
+++ main.clj
@@ -9,12 +9,15 @@
 
 (defn remove-first-tab "Removes the first tab character from a string." [x] (clojure.string/replace-first x #"\t" ""))
 
-(defn whitespace->parentheses "Converts a string of code from whitespace notation to parenthetical notation. Output is explicitly wrapped in a do-block for clarity." [l] (if (string? l) (print-str (conj (whitespace->parentheses (split-on-newlines l) 'do))) (map (fn [x] (concat (vec (first x)) (vec (whitespace->parentheses (map remove-first-tab (second x)))))) (partition-all 2 (partition-by indented? l)))))
+(defn whitespace->parentheses "Converts a string of code from whitespace notation to parenthetical notation. Output is explicitly wrapped in a do-block for clarity." [l] (if (string? l) (print-str (conj (whitespace->parentheses (split-on-newlines l)) 'do)) (map (fn [x] (concat (vec (first x)) (vec (whitespace->parentheses (map remove-first-tab (second x)))))) (partition-all 2 (partition-by indented? l)))))
 
-(defn whitespace->s-exprs "Converts from a string of whitespace-syntax code to an evaluable S-expression." [s] (-> s whitespace->parentheses read-line))
+(defn whitespace->s-exprs "Converts from a string of whitespace-syntax code to an evaluable S-expression." [s] (read-string (whitespace->parentheses s)))
 
 
 (comment "And below we have some examples.")
 (def s-expression-example '(s-expr->semantic-whitespace (defn tabs->parens [c] (map add-parens (rest (split-on-newlines c)) (list-deltas (code->numtabs c))))))
-(def semantic-whitespace-example (macroexpand-1 s-expression-example))
-(def back-to-s-expr-example (whitespace->s-exprs semantic-whitespace-example))
+(def example (macroexpand-1 s-expression-example))
+(def back-to-s-expr-example (whitespace->s-exprs example))
+
+
+(prn back-to-s-expr-example)
